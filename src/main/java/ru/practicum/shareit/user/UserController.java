866a7b90b08afb32db2dbb4e58240ed8664df598
ropgocTb <216320907+ropgocTb@users.noreply.github.com>
2026-02-04 16:20:33 +0300

    Added: controllers for items and users

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,36 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserService;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
 @RequestMapping(path = "/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService service;
+
+    @GetMapping("/{id}")
+    public UserDto getUser(@PathVariable(name = "id") Long id) {
+        return service.getUser(id);
+    }
+
+    @PostMapping
+    public UserDto addUser(@RequestBody UserDto userDto) {
+        return service.addUser(userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable(name = "id") Long id) {
+        service.deleteUser(id);
+    }
+
+    @PatchMapping("/{id}")
+    public UserDto editUser(@PathVariable(name = "id") Long id, @RequestBody UserDto userDto) {
+        return service.editUser(id, userDto);
+    }
 }
